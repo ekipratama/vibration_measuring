6b3fae17bddf6e7a206c1ecdf1d1062815495b0e
ekipratama <0989818@hr.nl>
2023-05-12 09:14:39 +0200

    realtime 2 graphs

--- realtimeplot2.py
+++ realtimeplot2.py
@@ -1,31 +1,56 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
 
-fig, ax = plt.subplots()
-xdata, ydata = [], []
-xdata2,ydata2= [], []
-ln, = plt.plot([], [], 'ro') 
-ln2, = plt.plot([], [], 'ro')
+def data_gen():
+    t = data_gen.t
+    cnt = 0
+    while cnt < 1000:
+        cnt+=1
+        t += 0.05
+        y1 = np.sin(2*np.pi*t) * np.exp(-t/10.)
+        y2 = np.cos(2*np.pi*t) * np.exp(-t/10.)
+        # adapted the data generator to yield both sin and cos
+        yield t, y1, y2
 
-def init():
-    ax.set_xlim(0, 2*np.pi)
-    ax.set_ylim(-1, 1)
-    return ln,ln2
+data_gen.t = 0
 
-def update(frame):
-    xdata.append(frame)
-    ydata.append(np.sin(frame))
-    ln.set_data(xdata, ydata)
+# create a figure with two subplots
+fig, (ax1, ax2) = plt.subplots(2,1)
 
-    xdata2.append(frame)
-    ydata2.append(np.cos(frame))
-    ln2.set_data(xdata2,ydata2)
-    
-    return ln,ln2
+# intialize two line objects (one in each axes)
+line1, = ax1.plot([], [], lw=2)
+line2, = ax2.plot([], [], lw=2, color='r')
+line = [line1, line2]
 
-ani = FuncAnimation(fig, update, frames=np.linspace(0, 2*np.pi, 128),
-                    init_func=init, blit=True)
+# the same axes initalizations as before (just now we do it for both of them)
+for ax in [ax1, ax2]:
+    ax.set_ylim(-1.1, 1.1)
+    ax.set_xlim(0, 5)
+    ax.grid()
 
+# initialize the data arrays 
+xdata, y1data, y2data = [], [], []
+def run(data):
+    # update the data
+    t, y1, y2 = data
+    xdata.append(t)
+    y1data.append(y1)
+    y2data.append(y2)
 
+    # axis limits checking. Same as before, just for both axes
+    for ax in [ax1, ax2]:
+        xmin, xmax = ax.get_xlim()
+        if t >= xmax:
+            ax.set_xlim(xmin, 2*xmax)
+            ax.figure.canvas.draw()
+
+    # update the data of both line objects
+    line[0].set_data(xdata, y1data)
+    line[1].set_data(xdata, y2data)
+
+    return line
+
+ani = animation.FuncAnimation(fig, run, data_gen, blit=True, interval=10,
+    repeat=False)
 plt.show()
